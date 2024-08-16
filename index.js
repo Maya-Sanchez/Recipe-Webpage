1ce63181516207c90873748c9352aa3a7f3a7813
Maya-Sanchez <sanchez.y.maya@gmail.com>
2024-08-16 14:49:05 -0400

    parent/child

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const header1 = document.getElementById("header")
 header1.style.backgroundColor="blue"
 // console.log(header)
 
-let recList = document.querySelector("#recipeList");
+let recList = document.querySelector('#recipeList');
 // console.log(recList)
-recList.style.backgroundColor="purple"
+recList.style.backgroundColor="purple";
 
-const parentItems = document.getElementById("#items1");
+const parentIng = document.getElementById('ing2');
 
-console.log(parentItems)
-// const childrenItem =parentItems.children;
+// console.log(parentItems);
+const childrenIng = parentIng.children;
 
-// console.log(parentItems)
-// console.log(childrenIndgredients)
+console.log(childrenIng);
