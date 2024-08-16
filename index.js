0e6601387301566f7c2a0c75ab1c025720ea3077
Maya-Sanchez <sanchez.y.maya@gmail.com>
2024-08-16 15:09:41 -0400

    append and CreateElement

--- index.js
+++ index.js
@@ -3,12 +3,50 @@ header1.style.backgroundColor="blue"
 // console.log(header)
 
 let recList = document.querySelector('#recipeList');
-// console.log(recList)
 recList.style.backgroundColor="purple";
+// console.log(recList)
 
-const parentIng = document.getElementById('ing2');
-
+//-----------------------------------------------------------------
+const parentIngList1 = document.getElementById('ing2');
 // console.log(parentItems);
-const childrenIng = parentIng.children;
 
-console.log(childrenIng);
+const childrenIng1 = parentIngList1.children;
+// console.log(childrenIng);
+
+// console.log(childrenIng1[0]);
+
+for (let i = 0; i < childrenIng1.length; i++){
+    console.log(childrenIng1[i]);
+}
+
+const chickenBuillon = document.createElement('li')
+chickenBuillon.textContent = "Chicken buillon"
+parentIngList1.appendChild(chickenBuillon);
+
+//---------------------------------------------------------
+
+const parentIngList2 = document.getElementById('ing2');
+// console.log(parentItems);
+
+const childrenIng2 = parentIngList2.children;
+// console.log(childrenIng);
+
+console.log(childrenIng2[0]);
+//---------------------------------------------------------
+
+const parentIngList3 = document.getElementById('ing2');
+// console.log(parentItems);
+
+const childrenIng3 = parentIngList3.children;
+// console.log(childrenIng);
+
+console.log(childrenIng3[0])
+
+//---------------------------------------------------------
+const parentIngList4 = document.getElementById('ing2');
+// console.log(parentItems);
+
+const childrenIng4 = parentIngList4.children;
+// console.log(childrenIng);
+
+console.log(childrenIng4[0])
