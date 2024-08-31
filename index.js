418531077500416d0d6ab78d5f85c10875582c59
Maya-Sanchez <sanchez.y.maya@gmail.com>
2024-08-31 17:17:57 -0400

    SBA additions

--- index.js
+++ index.js
@@ -124,3 +124,40 @@ function validate(){
         return false;
     }
 }
+
+//---------------------------------- Code for 308.A SBA
+fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+
+  const apiUrl = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+
+const contactForm = document.getElementById('contact-form');
+const responseMessage = document.getElementById('response-message');
+
+contactForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(contactForm);
+
+  const requestOptions = {
+    method: 'POST',
+    body: formData,
+  };
+
+  fetch("", requestOptions)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(data => {
+      responseMessage.textContent = data;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
+//------------------------------------------------------------------------------
